c6584c5988c56ac6a2ef9400522bf14899e060ef
hyeoKing <cksgurwkd12@naver.com>
2024-09-01 17:28:40 +0900

    [#2] 프로젝트 세팅 이슈 해결을 위해 스프링 부트 프로젝트 생성 - 빌드를 위한 파일들 포함 (build.gradle, gradlew, gradlew.bat ..) - SpringBootApplication을 적용할 main 클래스 작성 - gitignore 파일 포함

---
+++ src/main/java/com/teamhyeok/harmonyenglishacademy/HarmonyEnglishAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.teamhyeok.harmonyenglishacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyEnglishAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonyEnglishAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamhyeok/harmonyenglishacademy/HarmonyEnglishAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhyeok.harmonyenglishacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyEnglishAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
