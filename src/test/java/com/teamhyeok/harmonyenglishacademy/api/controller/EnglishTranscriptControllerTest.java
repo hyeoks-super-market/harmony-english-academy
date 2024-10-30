c597e6d0cb5ebc1add30d0f8ca3dd486bd92049c
hyeok <cksgurwkd12@naver.com>
2024-10-30 12:36:35 +0900

    [#3] 영어 스크립트 게시글 등록 통합 테스트에 컨트롤러 body 생성 방법을 String에서 create Class를 이용하는 방법으로 변경
    # EnglishTranscriptControllerTest
    - request 클래스 생성
    - objectMapper를 이용해 문자열 형태로 변환 후 mockMvc에 content로 넘겨줌

--- src/test/java/com/teamhyeok/harmonyenglishacademy/api/controller/EnglishTranscriptControllerTest.java
+++ src/test/java/com/teamhyeok/harmonyenglishacademy/api/controller/EnglishTranscriptControllerTest.java
@@ -1,31 +1,50 @@
 package com.teamhyeok.harmonyenglishacademy.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamhyeok.harmonyenglishacademy.api.repository.EnglishTranscriptRepository;
+import com.teamhyeok.harmonyenglishacademy.api.request.EnglishTranscriptCreate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@AutoConfigureMockMvc @SpringBootTest
 class EnglishTranscriptControllerTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    EnglishTranscriptRepository englishTranscriptRepository;
 
-    @DisplayName("게시글 등록 요청시 OK 헤더가 내려온다.")
+    @DisplayName("게시글 등록 요청시 IS_CREATED 헤더가 내려온다.")
     @Test
     void register() throws Exception{
+        // given
+        EnglishTranscriptCreate request = EnglishTranscriptCreate.builder()
+                                            .title("The Glee, season2 ep.3")
+                                            .content("the series ...@#!#")
+                                            .build();
+
+        String stringRequest = objectMapper.writeValueAsString(request);
 
         // expected
         mockMvc.perform(post("/api/v1/english-transcript")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"The Glee, season2 ep.3\", \"content\": \"the series ...@#!#\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(stringRequest)
                 )
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", "/api/v1/english-transcripts"))
@@ -38,17 +57,26 @@ class EnglishTranscriptControllerTest {
     @Test
     void register_title_is_required() throws Exception {
 
+        // given
+        EnglishTranscriptCreate request = EnglishTranscriptCreate.builder()
+                .title("")
+                .content("the series ...@#!#")
+                .build();
+
+        String stringRequest = objectMapper.writeValueAsString(request);
+
         // expected
         mockMvc.perform(post("/api/v1/english-transcript")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"\", \"content\": \"the series ...@#!#\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(stringRequest)
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value("400"))
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
-                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
+                .andExpect(jsonPath("$.validation.title").value("영어 스크립트 게시글 등록을 위해선 이름이 필요합니다."))
                 .andDo(print());
 
     }
+
 
 }
