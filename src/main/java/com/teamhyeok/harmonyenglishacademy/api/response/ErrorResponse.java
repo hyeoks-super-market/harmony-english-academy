40d53512897beb27dd1b2ac8a23eda344fce2f92
hyeok <cksgurwkd12@naver.com>
2024-10-30 12:48:38 +0900

    [#3] 영어 게시글 등록 요청 레벨에서 예외 발생시 응답 객체 생성을 빌더 이용토록 변경
    # 예외 발생시 응답 객체 생성을 빌더 이용토록 변경

--- src/main/java/com/teamhyeok/harmonyenglishacademy/api/response/ErrorResponse.java
+++ src/main/java/com/teamhyeok/harmonyenglishacademy/api/response/ErrorResponse.java
@@ -1,7 +1,7 @@
 package com.teamhyeok.harmonyenglishacademy.api.response;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,12 +17,18 @@ import java.util.Map;
  *     }
  * }
  */
-@Getter @RequiredArgsConstructor
+@Getter
 public class ErrorResponse {
 
     private final String code;
     private final String message;
     private final Map<String, String> validation = new HashMap<>();
+
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
     public void addValidation(String fieldName, String errorMessage) {
         validation.put(fieldName, errorMessage);
