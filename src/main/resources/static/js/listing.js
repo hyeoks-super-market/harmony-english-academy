9d8c1c674d1ce2e3df31d6cdbad05794b0e9cb1f
hyeok <cksgurwkd12@naver.com>
2024-11-22 02:13:15 +0900

    [#32] 각 영어 스크립트 게시글 이미지에 유튜브 썸네일 노출토록 처리 - 서버로부터 응답받은 유튜브 url에서 비디오 id를 추출해 해당하는 유튜브 썸네일 노출 시키도록 js 처리

--- src/main/resources/static/js/listing.js
+++ src/main/resources/static/js/listing.js
@@ -22,15 +22,18 @@ function renderTranscriptList(transcriptList) {
     $articlesBox.empty();
 
     transcriptList.forEach((transcript) => {
-        const { id, title, content, createdAt } = transcript;
+        const { id, title, content, createdAt, youtubeUrl } = transcript;
 
         const truncatedContent = content.length > 100 ? content.substring(0, 100) + '...' : content;
         const formattedDate = new Date(createdAt).toLocaleDateString();
 
+        const videoId = extractVideoId(youtubeUrl);
+        const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+
         const htmlData = `
         <div class="col">
             <div class="article-card h-100">
-                <img src="/images/announce/postIsNotReady.png" class="article-img-top" alt="Article Image">
+                <img src="${thumbnailUrl}" class="article-img-top" alt="Article Image">
                 <div class="article-body">
                     <a href="detail.html?id=${id}">
                         <h5 class="article-title">${title}</h5>
@@ -43,4 +46,10 @@ function renderTranscriptList(transcriptList) {
 
         $articlesBox.append(htmlData);
     });
+}
+
+function extractVideoId(youtubeUrl) {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/)|youtu\.be\/)([^&\n?#]+)/;
+    const match = youtubeUrl.match(regex);
+    return match && match[1] ? match[1] : null;
 }
