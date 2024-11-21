ddcf7573dec45fe40a9e02da0ff4966103ec0693
hyeok <cksgurwkd12@naver.com>
2024-11-22 02:09:24 +0900

    [#32] EnglishTranscript에 url 필드가 추가됨으로 data.sql, schema.sql에 스키마 정보와 youtubeUrl 테스트 데이터 정보 추가 - EnglishTranscript 테이블 생성시에 youbueUrl 필드 추가 - 테스트 데이터 생성을 위한 insert문에 테스트용 url인 'https://www.youtube.com/watch?v=a57hvjknIcQ'로 전부 채워지도록 처리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS english_transcript (
                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                   title VARCHAR(255) NOT NULL,
                                   content TEXT,
+                                  youtube_url VARCHAR(255) NOT NULL,
                                   created_at TIMESTAMP
                               );
