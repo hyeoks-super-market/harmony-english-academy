f9ef4917091e7b8d8fba9dbda41e45cac1f85a48
hyeok <cksgurwkd12@naver.com>
2024-11-22 02:02:23 +0900

    [#32] EnglishTranscript 생성 절차에 url에 대한 유효성 검증 기능 추가 - 유효한 유튜브 url인지 검증 - 비디오 id를 추출해서 GCP(GoogleCloudPlatform) api에 해당 video id의 영상이 실제로 존재하는지 체크 - 존재하지 않는 경우, 그 외의 오류에 대한 예외처리

--- src/main/java/com/teamhyeok/harmonyenglishacademy/api/service/EnglishTranscriptService.java
+++ src/main/java/com/teamhyeok/harmonyenglishacademy/api/service/EnglishTranscriptService.java
@@ -4,19 +4,29 @@ import com.teamhyeok.harmonyenglishacademy.api.domain.EnglishTranscript;
 import com.teamhyeok.harmonyenglishacademy.api.repository.EnglishTranscriptRepository;
 import com.teamhyeok.harmonyenglishacademy.api.request.EnglishTranscriptCreate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@RequiredArgsConstructor @Service
+@RequiredArgsConstructor @Service @Slf4j
 public class EnglishTranscriptService {
     private final EnglishTranscriptRepository englishTranscriptRepository;
 
     public void createTranscript(EnglishTranscriptCreate englishTranscriptCreate) {
+
+        validateYouTubeVideo(englishTranscriptCreate.getYoutubeUrl());
 
         englishTranscriptRepository.save(englishTranscriptCreate.toEntity());
 
@@ -36,6 +46,38 @@ public class EnglishTranscriptService {
 
         return pageOfEnglishTranscript.getContent();
 
+    }
+
+    private void validateYouTubeVideo(String youtubeUrl) {
+        String apiKey = System.getenv("YOUTUBE_API_KEY");
+        String videoId = extractVideoId(youtubeUrl);
+
+        String apiUrl = "https://www.googleapis.com/youtube/v3/videos?id=" + videoId + "&key=" + apiKey + "&part=id";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(apiUrl, String.class);
+
+        log.debug("youtube api 응답" + response);
+
+        try {
+            JSONObject json = new JSONObject(response.getBody());
+            JSONArray items = json.getJSONArray("items");
+            if (items.length() == 0) {
+                throw new IllegalArgumentException("존재하지 않는 YouTube 비디오입니다.");
+            }
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("YouTube API 응답을 처리하는 중 오류가 발생했습니다.");
+        }
+    }
+
+    private String extractVideoId(String youtubeUrl) {
+        String regex = "(?<=v=|youtu\\.be/|embed/)[^&\\n?#]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(youtubeUrl);
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            throw new IllegalArgumentException("유효한 YouTube URL이 아닙니다.");
+        }
     }
 }
 
